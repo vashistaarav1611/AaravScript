d341365d6f8bff3e184c61345cf02a14d63711d7
vashistaarav1611 <77904894+vashistaarav1611@users.noreply.github.com>
2021-08-20 18:11:15 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+//adding things functions
+function aaravID(id, name) {
+  document.getElementById(id).innerHTML = name;
+}
+function aaravIDadd(id, name) {
+  document.getElementById(id).innerHTML += name;
+}
+function aaravCL(id, name) {
+  document.getElementById(id).innerHTML = name;
+}
+function aaravCLadd(id, name) {
+  document.getElementById(id).innerHTML += name;
+}
+function aaravNA(id, name) {
+  document.getElementsByName(id).innerHTML = name;
+}
+function aaravNAadd(id, name) {
+  document.getElementsByName(id).innerHTML += name;
+}
